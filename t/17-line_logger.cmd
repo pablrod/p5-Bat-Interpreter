454a077a662ed5fee756042091792f72ba1e88cf
Pablo Rodríguez González <pablo.rodriguez.gonzalez@gmail.com>
2019-12-15 13:45:20 +0100

    Complete test for FOR /F

--- t/17-line_logger.cmd
+++ t/17-line_logger.cmd
@@ -8,14 +8,23 @@ set VALUE=6
 set GOTOLABEL=NO
 set VALUE=123456
 echo %VALUE:~-2%
+set VALUEFORKEY=something
 
 for %%m in (1,3,6) do if %%m GEQ %VALUE% set GOTOLABEL=YES
+
+@for /F "delims=" %%J in ('perl -E "my %%hash=qw/key %VALUEFORKEY%/; print @hash{key};"') do set KEY=%%J
 
 If %GOTOLABEL% EQU YES goto anotherlabel
 
 dir
 
 :anotherlabel
+
+goto anotherone
+
+deltree everything
+
+:anotherone
 
 
 If 130 GTR 14 goto label
